60a2f8d6f3f5e56f6ca7666fe8ced796e20fe707
Jinwoo Je <jeff.jw.je@gmail.com>
2024-07-10 02:07:31 +0000

    user post translation WIP

--- app/routes.py
+++ app/routes.py
@@ -18,6 +18,7 @@ from urllib.parse import urlsplit
 from datetime import datetime, timezone
 from app.email import send_password_reset_email
 from langdetect import detect, LangDetectException
+from app.translate import translate
 
 @app.before_request
 def before_request():
@@ -201,3 +202,11 @@ def reset_password(token):
         flash(_('Your password has been reset.'))
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    data = request.get_json()
+    return {'text': translate(data['text'],
+                             data['source_language'],
+                             data['dest_language'])}
