ce949f92c761bbee08fab2cca439ec3504d2d50d
Jinwoo Je <jeff.jw.je@gmail.com>
2024-07-19 13:24:15 +0000

    better app structure - main WIP

---
+++ app/main/routes.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timezone
+from flask import render_template, flash, redirect, url_for, request, g, current_app
+from flask_login import current_user, login_required
+from flask_babel import _, get_locale
+import sqlalchemy as sa
+from langdetect import detect, LangDetectException
+from app import db
+from app.main.forms import EditProfileForm, EmptyForm, PostForm
+from app.models import User, Post
+from app.translate import translate
+from app.main import bp
+
+
+@bp.before_app_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
+    g.locale = str(get_locale())
