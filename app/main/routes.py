bc0e194fb2ce8d55c4fc3dde4e7dddc6fd76930d
Jinwoo Je <jeff.jw.je@gmail.com>
2024-07-25 12:58:48 +0000

    better app structure main routes.py WIP

--- app/main/routes.py
+++ app/main/routes.py
@@ -66,4 +66,20 @@ def user(username):
     form = EmptyForm()
     return render_template('user.html', user=user, posts=posts.items, next_url=next_urp, prev_url=prev_url, form=form)
 
+@bp.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_sumit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash(_('Your changes have been saved.'))
+        return redirect(url_for('main.dedit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title=_('Edit Profile'), form=form)
+
 // start here
+
