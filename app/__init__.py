fb8fd343406d78a62e4da0fca687ea9a000a0678
Jinwoo Je <jeff.jw.je@gmail.com>
2024-07-31 11:28:21 +0000

    better app structure app init WIP

--- app/__init__.py
+++ app/__init__.py
@@ -11,3 +11,37 @@ from flask_babel import Babel, lazy_gettext as _l
 from config import Config
 
 
+def get_locale():
+    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
+
+db = SQLAlchemy()
+migrage = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = _l('Please log in to access this page.')
+mail = Mail()
+moment = Moment()
+babel = Babel()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    babel.init_app(app, locale_selector=get_locale)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    ## start here after creating CLI bp
